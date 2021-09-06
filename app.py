f85d621a0058e48d120673fe2c19bc8395db4eed
kalai vani <harish123809@gmail.com>
2021-09-06 15:53:48 +0530

    added env

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import *
 from flask_pymongo import pymongo
 from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+load_dotenv()
+PSW=os.getenv("PSW")
 
 app=Flask(__name__)
-CONNECTION_STRING ='mongodb+srv://Harish:harish@cluster0.hpddk.mongodb.net/user?retryWrites=true&w=majority'
+CONNECTION_STRING =f"mongodb+srv://Harish:{PSW}@cluster0.hpddk.mongodb.net/user?retryWrites=true&w=majority"
 
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('users')
@@ -23,12 +27,11 @@ def check():
     user_db=db.userDB.find_one({"username":username})
     if user_db:
         if(user_db["username"]==username and user_db["password"]==password):
-            return redirect(url_for('user', username=username))
+            return render_template("database.html", username=username, text=user_db["data"])
         else:
             return "Incorrect username or password"
     else:
         return "Incorrect username or password"
-
 
 ###################################################################################
 @app.route('/register')
@@ -43,15 +46,6 @@ def register_user():
     return redirect("/")
 
 #################################################################################
-
-@app.route('/user/<username>')
-def user(username):
-    ch=db.userDB.find_one({"username":username})
-    if(ch['username']==username):
-        return render_template("database.html", username=username, text=ch["data"])
-    else:
-        db.userDB.insert_one({"username":username})
-        return render_template('database.html',username=username, text=ch["data"])
 
 @app.route('/database',methods=['POST'])
 def database():
